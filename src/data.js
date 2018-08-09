3724f0a59a168815a220a76705273ea9396da7ef
Nate Worcester <nate.worcester@gmail.com>
2018-08-09 13:17:50 -0500

    initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Frame it out",
+    user: {
+      id: 1,
+      name: "Some Guy",
+      avatar: "https://randomuser.me/api/portraits/men/47.jpg"
+    }
+  },{
+    id: 2,
+    title: "Make it look nice",
+    user: {
+      id: 2,
+      name: "Random Gal",
+      avatar: "https://randomuser.me/api/portraits/women/68.jpg"
+    }
+  },{
+    id: 2,
+    title: "Add some sizzle",
+    user: {
+      id: 2,
+      name: "Another Guy",
+      avatar: "https://randomuser.me/api/portraits/men/46.jpg"
+  }
+];
